6dca076e08eeebb968685e695f9ce0bca571ef33
shooter <shooter.hero@gmail.com>
2019-09-25 21:25:13 +0800

    call system command

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,23 +1,36 @@
 'use strict';
-const exec = require('child_process').exec;
 
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
+
   async index() {
+
+    function execShellCommand(cmd) {
+      // https://medium.com/@ali.dev/how-to-use-promise-with-exec-in-node-js-a39c4d7bbf77
+
+      const exec = require('child_process').exec;
+
+      return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+          if (error) {
+            console.err(error);
+          }
+
+          resolve(stdout ? stdout : stderr);
+
+        });
+      });
+    }
+
     const { ctx } = this;
 
-    const child = exec('ls -l | wc -l',
-        (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
+    var adb = "/Users/shooter/Library/Android/sdk-macosx/platform-tools/adb";
+    const devices = await execShellCommand(adb + " devices");
+    const tap = await execShellCommand(adb + " shell input tap 540 2022");
 
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-    });
+    ctx.body = devices
 
-    ctx.body = 'hi, egg';
   }
 }
 

8a35c4fe327936c1c5fe462da8103d68e3087f19
shooter <shooter.hero@gmail.com>
2019-09-25 18:08:35 +0800

    nodejs call system command

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,10 +1,22 @@
 'use strict';
+const exec = require('child_process').exec;
 
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+
+    const child = exec('ls -l | wc -l',
+        (error, stdout, stderr) => {
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+    });
+
     ctx.body = 'hi, egg';
   }
 }
